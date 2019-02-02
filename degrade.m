0cd3706226f7e126d493b73e6af45741b02617a0
hbmcneil <38723223+hbmcneil@users.noreply.github.com>
2019-02-02 19:54:59 +0100

    Who Knows

--- degrade.m
+++ degrade.m
@@ -4,49 +4,59 @@ function [c_aq, n_napl] = degrade(c_aq_old, n_napl_old, fcfb, fcfa, fbfa, poros,
 % reaction of napl compounds with oxygen (electron donor) facilitated by
 % microorganisms reaction
 
-% initialize concentrations
+% initialize concentrations for equilibration
 c_aq = c_aq_old;
 n_napl = n_napl_old;
 
+% initialize total concentrations for reaction
 c_a_tot = c_aq_old(:,1:24);
 c_b_tot = c_aq_old(:,25);
-c_a = c_a_tot;
-c_b = c_b_tot;
+
+% initial aqueous concentrations for reaction 
+c_a = c_aq_old(:,1:24);
+c_b = c_aq_old(:,25);
 c_c = zeros.*(c_aq_old(:,1:24));
 
 % loop over all cells in model domain
 for i =1:size(c_aq_old,1) 
-    % skip over cell if oxygen or napl is depleted
-     Xi = n_napl(i,:)./sum(n_napl(i,:));
     
-    if c_b_tot(i,:) <=0 || sum(c_a_tot(i,:)) <= 0 
+    % skip over cell if oxygen or napl is depleted
+    if c_b(i,:) <=0 || sum(c_a(i,:)) <= 0 
         % skip the iteration, on to next cell
         continue
     end
-        
-    % enter reaction within cell if oxygen or napl is present
+  
+   % enter reaction within cell if oxygen or napl is present
    while sum(c_a(i,:)) > 0 && c_b(i,:) > 0 
+     
+    % React hydrocarbons as ratio of compounds present in aqueous phase
+    Xi = c_a_tot(i,:)./sum(c_a_tot(i,:));   
     
+    % reaction that occurs if napl compounds are greater than oxygen    
     if sum(fcfa.*c_a_tot(i,:)) < sum(Xi.*fcfb.*c_b_tot(i,:))
+        % not sure if molar fraction is needed here since summing it 
+       
        c_a(i,:) = zeros.*c_a_tot(i,:);
-       c_b(i,:) = sum(Xi.*c_b_tot(i,:) - fbfa.*c_a_tot(i,:));
+       c_b(i,:) = sum(Xi.*(c_b_tot(i,:) - fbfa.*c_a_tot(i,:)));
        c_c(i,:) = Xi.*fcfa.*c_a_tot(i,:);
        
-    % enter reaction for second scenario within cell if oxygen or napl is present
+    % reaction that occurs if napl compounds are greater than oxygen
     else
        c_a(i,:) = c_a_tot(i,:) - Xi.*(1./fbfa).*c_b_tot(i,:);
        c_b(i,:) = zeros.*c_b_tot(i,:);
        c_c(i,:) = Xi.*fcfb.*c_b_tot(i,:);
     end
     
-    c_a_tot(i,:) = c_a(i,:) + (1./fcfa).*c_c(i,:);
-    c_b_tot(i,:) = c_b(i,:) + sum((1./fcfb).*c_c(i,:));
-    
+     c_a_tot(i,:) = c_a(i,1:24) + (1./fcfa).*c_c(i,:);
+     c_b_tot(i,:) = c_b(i,:) + sum((1./fcfb).*c_c(i,:));
+  
    end
    
-   [a,n_napl] = equilibrate(c_a(i,1:24), n_napl_old, poros, Si, rho, MW, dx, A);
+   % equilibration following reaction 
+    [a,n_napl(i,1:24)] = equilibrate(c_a(i,1:24), n_napl_old(i,1:24), poros, Si, rho, MW, dx, A);
     c_aq(i,1:24) = a;
     c_aq(i,25) = c_b(i,:);
+    
 end
 
 end
